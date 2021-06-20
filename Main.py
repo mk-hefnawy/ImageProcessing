815253ba05aff73dbaa98f58f89241b5da4a3f61
mk-hefnawy <69528783+MeLeGy-Also@users.noreply.github.com>
2021-06-20 18:49:13 +0200

    Final

--- Main.py
+++ Main.py
@@ -1,14 +1,10 @@
 import shutil
 import sys
-
-from PIL import Image
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPixmap, QIcon, QFont, QImage
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLineEdit, QLabel, QVBoxLayout, QPushButton, QTextEdit
 
 import os.path
-
-from PyQt5.uic.properties import QtGui
 from cv2 import cv2
 
 import Utils
@@ -18,6 +14,9 @@ class Window(QMainWindow):
     utils = Utils.Utils()
 
     counter = 0
+    counter_load = 0
+    convert_clicked = 0
+    translated_clicked = 0
 
     dynamic_path = ""
 
@@ -27,8 +26,8 @@ class Window(QMainWindow):
     image = None
     lb = None
     path_line_edit = None
-    convert_to_text_btn, increase_brightness, fill_gaps, remove_noise = None, None, None, None
-    converted_text_txt_view, translated_text_view = None, None
+    convert_to_text_btn, increase_brightness, fill_gaps, remove_noise, unglue = None, None, None, None, None
+    converted_text_txt_view, translated_text_view, converted_text, translated_text = None, None, None, None
 
     translate_btn, spell_btn, spell_translated_btn = None, None, None
     v_layout = QVBoxLayout()
@@ -38,6 +37,7 @@ class Window(QMainWindow):
 
         # setting title
         self.setWindowTitle("Reading Made Easy")
+        # setting icon
         icon = QIcon("D:\\4thComputer\\2ndTerm\\Image\\Project\\icon1.png")
         self.setWindowIcon(icon)
 
@@ -53,82 +53,89 @@ class Window(QMainWindow):
 
     # method for components
     def ui_components(self):
-
+        # label of the line edit
         path_label = QLabel("Image Path", self)
         path_label.setGeometry(40, 0, 120, 60)
         path_label.setWordWrap(True)
 
         self.v_layout.addWidget(path_label)
 
+        # line edit where the user enters the path of the image
         self.path_line_edit = QLineEdit("", self)
         self.path_line_edit.setGeometry(40, 40, 420, 30)  # x ,y , width, height
 
         self.v_layout.addWidget(self.path_line_edit)
 
+        # Load button
         load_btn = QPushButton("Load", self)
         load_btn.setGeometry(480, 40, 60, 30)
         load_btn.clicked.connect(lambda: self.on_load_clicked())
 
+        # preview of the loaded image
         self.lb = QLabel("Loaded Image", self)
         self.lb.setVisible(False)
 
+        # preview of the loaded image
         self.label = QLabel("Modified Image", self)
         self.label.setVisible(False)
 
+        #  convert to text button
         self.convert_to_text_btn = QPushButton("Convert to text", self)
         self.convert_to_text_btn.setVisible(False)
 
+        #  increase brightness button
         self.increase_brightness = QPushButton("Increase Brightness", self)
         self.increase_brightness.setVisible(False)
 
+        #  fill gaps button
         self.fill_gaps = QPushButton("Fill Gaps", self)
         self.fill_gaps.setVisible(False)
 
+        #  Unglue button
+        self.unglue = QPushButton("Unglue", self)
+        self.unglue.setVisible(False)
+
+        #  remove noise button
         self.remove_noise = QPushButton("Remove Noise", self)
         self.remove_noise.setVisible(False)
 
-        """self.layout = QtGui.QHBoxLayout()  # layout for the central widget
-        self.widget = QtGui.QWidget(self)  # central widget
-        self.widget.setLayout(self.layout)
-
-        self.processing_type = QtGui.QButtonGroup(self.widget)
-
-        self.increase_brightness = QtGui.QRadioButton("Increase brightness")
-        self.processing_type.addButton(self.increase_brightness)
-
-        self.fill_gaps = QtGui.QRadioButton("Fill gaps")
-        self.processing_type.addButton(self.fill_gaps)
-
-        self.remove_noise = QtGui.QRadioButton("Remove noise")
-        self.processing_type.addButton(self.remove_noise)"""
-
+        #  text view of the converted text
         self.converted_text_txt_view = QTextEdit("", self)
         self.converted_text_txt_view.setVisible(False)
         self.converted_text_txt_view.setReadOnly(True)
 
+        #  translate button
         self.translate_btn = QPushButton("Translate", self)
         self.translate_btn.setVisible(False)
 
+        # text view of the translated text
         self.translated_text_view = QTextEdit("", self)
         self.translated_text_view.setVisible(False)
         self.translated_text_view.setReadOnly(True)
 
+        # spell the converted text button
         self.spell_btn = QPushButton("Spell", self)
         self.spell_btn.setVisible(False)
 
+        # spell the translated text button
         self.spell_translated_btn = QPushButton("Spell", self)
         self.spell_translated_btn.setVisible(False)
 
+    # call back method of load button
     def on_load_clicked(self):
         self.counter = 0
+        self.counter_load += 1
         self.utils.counter_zero()
         shutil.rmtree('D:\\4thComputer\\2ndTerm\\Image\\Project\\A\\')
         path = os.path.join('D:\\4thComputer\\2ndTerm\\Image\\Project\\', 'A')
         os.mkdir(path)
 
-        self.edit_line_text = self.path_line_edit.text()
+        self.label.setVisible(False)
+        self.converted_text_txt_view.setVisible(False)
+        self.translated_text_view.setVisible(False)
 
-        print(self.edit_line_text)
+        # input validation
+        self.edit_line_text = self.path_line_edit.text()
         if len(self.edit_line_text) == 0:
             message = "Please enter image path"
             self.utils.show_message(message)
@@ -140,154 +147,172 @@ class Window(QMainWindow):
         elif os.path.isfile(self.edit_line_text):
             print("Valid path")
 
-            self.load_image(self.edit_line_text)
+            self.display_image(self.edit_line_text)
 
         else:
-            message = "hhhh"
+            message = " "
             self.utils.show_message(message)
 
-    def load_image(self, path):
-        self.display_image(path)
-
+    # display the preview of the input image
     def display_image(self, path):
         self.lb.setVisible(True)
         self.lb.setGeometry(40, 100, 500, 500)
 
         pixmap = QPixmap(path)
-        image = cv2.imread(path)
 
         self.lb.setPixmap(pixmap.scaled(self.lb.size()))
-        self.prepare_convert_btn(self.edit_line_text)
-        self.prepare_processing_btns(image)
+        if self.counter_load <= 1:
+            self.prepare_convert_btn()
+            self.prepare_processing_btns()
 
+    # display the image after some process
     def display_modified_image(self, image_path):
         self.label.setVisible(True)
         self.label.setGeometry(600, 100, 500, 500)
-        #  img = QtGui.QImage(image, image.shape[1], image.shape[0], image.shape[1] * 3, QtGui.QImage.Format_RGB888)
-        #  img = self.utils.numpyQImage(image)
         pixMap = QPixmap(image_path)
         self.label.setPixmap(pixMap.scaled(self.label.size()))
 
-
-    def prepare_convert_btn(self, path):
+    # showing the convert to text button
+    def prepare_convert_btn(self):
         self.convert_to_text_btn.setGeometry(1200, 40, 100, 30)
         self.convert_to_text_btn.setVisible(True)
-        self.convert_to_text_btn.clicked.connect(lambda: self.on_convert_btn_clicked(path))
+        self.convert_to_text_btn.clicked.connect(lambda: self.on_convert_btn_clicked())
 
-    def prepare_processing_btns(self, image):
+    # showing the increase brightness, fill gaps, remove noise, and unglue buttons
+    def prepare_processing_btns(self):
         self.increase_brightness.setGeometry(600, 40, 120, 30)
         self.increase_brightness.setVisible(True)
-        self.increase_brightness.clicked.connect(lambda: self.on_increase_brightness_btn_clicked(image))
+        self.increase_brightness.clicked.connect(lambda: self.on_increase_brightness_btn_clicked())
 
         self.fill_gaps.setGeometry(750, 40, 120, 30)
         self.fill_gaps.setVisible(True)
-        self.fill_gaps.clicked.connect(lambda: self.on_fill_gaps_btn_clicked(image))
+        self.fill_gaps.clicked.connect(lambda: self.on_fill_gaps_btn_clicked())
 
-        self.remove_noise.setGeometry(900, 40, 120, 30)
+        self.unglue.setGeometry(900, 40, 120, 30)
+        self.unglue.setVisible(True)
+        self.unglue.clicked.connect(lambda: self.on_unglue_btn_clicked())
+
+        self.remove_noise.setGeometry(1050, 40, 120, 30)
         self.remove_noise.setVisible(True)
-        self.remove_noise.clicked.connect(lambda: self.on_remove_noise_btn_clicked(image))
+        self.remove_noise.clicked.connect(lambda: self.on_remove_noise_btn_clicked())
 
-    def on_convert_btn_clicked(self, path):
+    # call back function of convert button
+    def on_convert_btn_clicked(self):
         if self.counter > 0:
             path = self.updated_path
-        print("PATH   " + path)
+        else:
+            path = self.edit_line_text
         image = cv2.imread(path)
-        converted_text = self.utils.convert(image)
+        self.converted_text = self.utils.convert(image)
         self.converted_text_txt_view.setGeometry(1200, 100, 500, 100)
-        #  self.converted_text_txt_view.setWindowTitle("Converted Text")
         self.converted_text_txt_view.setVisible(True)
-        self.converted_text_txt_view.setText(converted_text)
+        self.converted_text_txt_view.setText(self.converted_text)
         self.converted_text_txt_view.setAlignment(Qt.AlignTop)
         self.converted_text_txt_view.setStyleSheet("padding: 5px")
         self.converted_text_txt_view.setFont(QFont("New Times", 12))
-        self.prepare_translate_and_spell(converted_text)
+        if self.convert_clicked == 0:
+            self.prepare_translate_and_spell()
 
-    def on_increase_brightness_btn_clicked(self, image):
-        print("on_increase_brightness_btn_clicked"+str(self.counter))
-        img_path = ""
-        if self.counter == 0:
-            img_path = self.utils.increase_brightness(img=image)
+        self.convert_clicked += 1
+
+    # call back function of increase brightness button
+    def on_increase_brightness_btn_clicked(self):
+        if self.counter > 0:
+            path = self.updated_path
+            new_path = self.utils.increase_brightness(path)
         else:
-            img_path = self.utils.increase_brightness(img_path=self.updated_path)
-            print("Updated Path" + self.updated_path)
-        self.display_modified_image(img_path)
-        self.updated_path = img_path
+            path = self.edit_line_text
+            new_path = self.utils.increase_brightness(path)
+
+        self.display_modified_image(new_path)
+        self.updated_path = new_path
 
         self.counter += 1
 
-    def on_fill_gaps_btn_clicked(self, image):
-        print("on_fill_gaps_btn_clicked"+str(self.counter))
-        img_path = ""
-        if self.counter == 0:
-            img_path = self.utils.close_gaps(img=image)
+    # call back function of fill gaps button
+    def on_fill_gaps_btn_clicked(self):
+        if self.counter > 0:
+            path = self.updated_path
+            new_path = self.utils.close_gaps(path, iterations=self.counter)
         else:
-            img_path = self.utils.close_gaps(img_path=self.updated_path, iterations=self.counter)
-        print("From OnFillGaps")
-        self.display_modified_image(img_path)
-        self.updated_path = img_path
+            path = self.edit_line_text
+            new_path = self.utils.close_gaps(path, iterations=self.counter)
 
+        self.display_modified_image(new_path)
+        self.updated_path = new_path
 
         self.counter += 1
 
-    def on_remove_gaps_btn_clicked(self, image):
-        pass
-
-    def on_remove_noise_btn_clicked(self, image):
-        print("on_remove_noise_btn_clicked"+str(self.counter))
-        img_path = ""
-        if self.counter == 0:
-            img_path = self.utils.remove_noise(img=image)
+    # call back function of unglue button
+    def on_unglue_btn_clicked(self):
+        if self.counter > 0:
+            path = self.updated_path
+            new_path = self.utils.open_gaps(path, iterations=self.counter)
         else:
-            img_path = self.utils.remove_noise(img_path=self.updated_path)
-        print("From OnRemoveNoise")
-        self.display_modified_image(img_path)
-        self.updated_path = img_path
+            path = self.edit_line_text
+            new_path = self.utils.open_gaps(path, iterations=self.counter)
 
-
+        self.display_modified_image(new_path)
+        self.updated_path = new_path
 
         self.counter += 1
 
+    # call back function of remove noise button
+    def on_remove_noise_btn_clicked(self):
+        if self.counter > 0:
+            path = self.updated_path
+            new_path = self.utils.remove_noise(path)
+        else:
+            path = self.edit_line_text
+            new_path = self.utils.remove_noise(path)
+        self.display_modified_image(new_path)
+        self.updated_path = new_path
+        self.counter += 1
 
-    def prepare_translate_and_spell(self, converted_text):
+    # show translate and spell (converted text) button
+    def prepare_translate_and_spell(self):
         self.translate_btn.setGeometry(1200, 220, 100, 30)
         self.translate_btn.setVisible(True)
-        self.translate_btn.clicked.connect(lambda: self.on_translate_clicked(converted_text))
+        self.translate_btn.clicked.connect(lambda: self.on_translate_clicked())
 
         self.spell_btn.setGeometry(1330, 220, 100, 30)
         self.spell_btn.setVisible(True)
 
-        #  Todo
         dest_language = 'en'
-        self.spell_btn.clicked.connect(lambda: self.on_spell_clicked(converted_text, dest_language))
+        self.spell_btn.clicked.connect(lambda: self.on_spell_clicked(dest_language))
 
-    def on_translate_clicked(self, converted_text):
+    # call back function of translate button
+    def on_translate_clicked(self):
         dest_language = 'ar'
-        translated_text = self.utils.translate(converted_text, dest_language)
+        self.translated_text = self.utils.translate(self.converted_text, dest_language)
         self.translated_text_view.setGeometry(1200, 280, 500, 100)
 
         self.translated_text_view.setVisible(True)
-        self.translated_text_view.setText(translated_text)
+        self.translated_text_view.setText(self.translated_text)
         self.translated_text_view.setAlignment(Qt.AlignTop)
         self.translated_text_view.setStyleSheet("padding: 5px")
         self.translated_text_view.setFont(QFont("New Times", 12))
+        if self.translated_clicked == 0:
+            self.prepare_spell_translated()
 
-        self.prepare_spell_translated(translated_text)
+        self.translated_clicked += 1
 
-    def on_spell_clicked(self, converted_text, dest_language):
-        self.utils.spell(converted_text, dest_language)
+    # call back function of spell (converted text) button
+    def on_spell_clicked(self, dest_language):
+        self.utils.spell(self.converted_text, dest_language)
 
-    def prepare_spell_translated(self, translated_text):
+    # show spell (translated text) button
+    def prepare_spell_translated(self):
 
         self.spell_translated_btn.setGeometry(1200, 400, 100, 30)
         self.spell_translated_btn.setVisible(True)
-
-        # Todo
         dest_language = 'ar'
         self.spell_translated_btn.clicked.connect(
-            lambda: self.on_spell_translated_clicked(translated_text, dest_language))
+            lambda: self.on_spell_translated_clicked(dest_language))
 
-    def on_spell_translated_clicked(self, translated_text, dest_language):
-        self.utils.spell(translated_text, dest_language)
+    # call back function of spell (translated text) button
+    def on_spell_translated_clicked(self, dest_language):
+        self.utils.spell(self.translated_text, dest_language)
 
 
 # create pyqt5 app
--- Utils.py
+++ Utils.py
@@ -14,37 +14,37 @@ from pytesseract import pytesseract
 class Utils:
     counter = 0
 
-
     def __init__(self):
         self.dtype = None
 
     def counter_zero(self):
         self.counter = 0
 
+    # show a message to the user
     def show_message(self, message):
         msg = QMessageBox()
         msg.setIcon(QMessageBox.Information)
         msg.setGeometry()
         msg.setText(message)
         msg.setWindowTitle("File not found :D")
-        #  msg.setDetailedText("The details are as follows:")
         msg.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
         msg.exec_()
-        #  msg.buttonClicked.connect(msgbtn)
 
+    # convert the input image to text using tesseract
     def convert(self, img):
         path_to_tesseract = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
         pytesseract.tesseract_cmd = path_to_tesseract
         text = pytesseract.image_to_string(img)
-        print("OCR" + text[:-1])
         return text
 
+    # translate the text
     def translate(self, converted_text, dest_language):
         trans = Translator()
         t = trans.translate(converted_text, dest=dest_language)
         print(googletrans.LANGUAGES)
         return t.text
 
+    # spell the text
     def spell(self, text, dest_language):
         myobj = gTTS(text=text, lang=dest_language, slow=False)
         # Saving the converted audio in a mp3 file named welcome
@@ -53,11 +53,9 @@ class Utils:
         playsound("test.mp3")
         os.remove("test.mp3")
 
-    def increase_brightness(self, img="", value=20, img_path=""):
-        print("Util" + str(self.counter))
-        if self.counter > 0:
-            img = cv2.imread(img_path)
-        print("From Increase Brightness Util")
+    # increase the brightness of the image
+    def increase_brightness(self, img_path, value=20):
+        img = cv2.imread(img_path)
         hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
         h, s, v = cv2.split(hsv)
 
@@ -70,73 +68,39 @@ class Utils:
         #  writing the image to disk
         path = "D:\\4thComputer\\2ndTerm\\Image\\Project\\A\\" + str(self.counter) + ".jpg"
         cv2.imwrite(path, image)
-        print("End of increase brightness utility")
         self.counter += 1
         return path
 
-    def dilation(self, img="", threshold=100, iterations=1, img_path=""):
-        print("Util" + str(self.counter))
-        if self.counter > 0:
-            img = cv2.imread(img_path)
-        grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        BW = 255 - cv2.threshold(grey, threshold, 255, cv2.THRESH_BINARY)[1]
-        square_kernel = np.ones((3, 3), np.uint8)
-        new_img = cv2.dilate(BW, square_kernel, iterations=iterations)
-        #  writing the image to disk
-        path = "D:\\4thComputer\\2ndTerm\\Image\\Project\\A\\" + str(self.counter) + ".jpg"
-        cv2.imwrite(path, new_img)
-        print("End of dilation utility")
-        self.counter += 1
-        return path
-
-    def close_gaps(self, img="", iterations = 1, img_path=""):
-        if self.counter > 0:
-            img = cv2.imread(img_path)
+    # fill the gaps of the image
+    def close_gaps(self, img_path, iterations=1):
+        img = cv2.imread(img_path)
         square_kernel = np.ones(((2 * iterations) + 1, (2 * iterations) + 1), np.uint8)
         out = cv2.dilate(cv2.erode(img, square_kernel, iterations=1), square_kernel, iterations=1)
         path = "D:\\4thComputer\\2ndTerm\\Image\\Project\\A\\" + str(self.counter) + ".jpg"
         cv2.imwrite(path, out)
-        print("End of close gaps utility")
         self.counter += 1
         return path
 
-    def open_gaps(self, img="", iterations=1, img_path=""):
-        if self.counter > 0:
-            img = cv2.imread(img_path)
+    # unglue connected parts of the image
+    def open_gaps(self, img_path, iterations=1):
+        img = cv2.imread(img_path)
         square_kernel = np.ones(((2 * iterations) + 1, (2 * iterations) + 1), np.uint8)
         out = cv2.erode(cv2.dilate(img, square_kernel, iterations=1), square_kernel, iterations=1)
         path = "D:\\4thComputer\\2ndTerm\\Image\\Project\\A\\" + str(self.counter) + ".jpg"
         cv2.imwrite(path, out)
-        print("End of open gaps utility")
         self.counter += 1
         return path
 
-    def erosion(self, img="", threshold=100, iterations=1, img_path=""):
-        print("Util" + str(self.counter))
-        if self.counter > 0:
-            img = cv2.imread(img_path)
-        grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        BW = 255 - cv2.threshold(grey, threshold, 255, cv2.THRESH_BINARY)[1]
-        square_kernel = np.ones((3, 3), np.uint8)
-        new_img = cv2.erode(BW, square_kernel, iterations=iterations)
-        #  writing the image to disk
-        path = "D:\\4thComputer\\2ndTerm\\Image\\Project\\A\\" + str(self.counter) + ".jpg"
-        cv2.imwrite(path, new_img)
-        print("End of erosion utility")
-        self.counter += 1
-        return path
-
-    def remove_noise(self, img="", img_path=""):
-        print("Util" + str(self.counter))
-        if self.counter > 0:
-            img = cv2.imread(img_path)
+    # remove noise from the image
+    def remove_noise(self, img_path):
+        img = cv2.imread(img_path)
         img = cv2.medianBlur(img, 3)  # 3
-
         path = "D:\\4thComputer\\2ndTerm\\Image\\Project\\A\\" + str(self.counter) + ".jpg"
         cv2.imwrite(path, img)
         self.counter += 1
         return path
 
+    # convert numpy array representation of an image to QImage
     def numpyQImage(self, image):
         qImg = QtGui.QImage()
         if image.dtype == np.uint8:
